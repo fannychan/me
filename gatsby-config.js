355d2793450117b97059cf067ef27dc8f0124ca7
Fanny Chan <fanny@byteslice.se>
2021-01-15 15:05:16 +0100

    Updated config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,28 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-transformer-json`,
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-typescript`,
       options: {
         isTSX: true, // defaults to false
